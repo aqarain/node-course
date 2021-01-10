b3571bfba5ac6ea4f399085bf7a70314c9ffb664
Atif Qayyum <aqarain92@gmail.com>
2021-01-10 18:16:24 +0500

    updated environment variable key

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 // "task-manager-api" is the db name
-mongoose.connect(process.env.MONGOD_URL, {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useCreateIndex: true
 });
